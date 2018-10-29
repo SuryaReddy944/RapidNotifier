1350bd4f783d6b621797b87315b96870554d3f79
Surya <nsuryanarayanareddyn@gmail.com>
2018-10-29 22:30:49 +0530

    Initial implementation of alaram list

---
+++ app/src/main/java/com/sg/rapid/Models/AlaramData.java
@@ -0,0 +1,62 @@
+package com.sg.rapid.Models;
+
+public class AlaramData {
+
+    private String year;
+    private String date;
+    private String time;
+    private String title;
+    private String description;
+    private String info;
+
+
+    public String getYear() {
+        return year;
+    }
+
+    public void setYear(String year) {
+        this.year = year;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getInfo() {
+        return info;
+    }
+
+    public void setInfo(String info) {
+        this.info = info;
+    }
+
+
+}
